1cbf31ba594d12cc7e1bd8532a8f88e39e572eea
Mohamed Ashraf <mohammed.ashraaf.15@gmail.com>
2018-06-16 13:54:20 +0200

    Create new data source that deals with xml api

---
+++ dataSource/API/XMLDataSource.php
@@ -0,0 +1,47 @@
+<?php
+
+class XMLDataSource extends APIBasic
+{
+    /**
+     * XMLDataSource constructor.
+     * @param $url
+     * @param array $additionalOptions
+     */
+    public function __construct($url, $additionalOptions = [])
+    {
+        $this->url = $url;
+
+        $this->additionalOptions = $additionalOptions;
+    }
+
+    /**
+     * Gets data from the source
+     * @param array $additionalOptions
+     * @return array
+     */
+    public function getData(array $additionalOptions = [])
+    {
+        // add request options
+        $this->addRequestOptions($additionalOptions);
+
+        // make the request
+        $response = $this->makeRequest();
+
+        // return the data
+        return $this->handleResponse($response, new XMLFormat());
+
+    }
+
+    /**
+     * Handles the response as needed
+     * @param $response
+     * @param FormatDataInterface $formatData
+     * @return mixed
+     */
+    public function handleResponse($response, FormatDataInterface $formatData)
+    {
+        // formats the results to array to match the expected return data type
+        return $formatData->format($response);
+    }
+
+}
