bc99256aab67fe3de97585e51b6a9506254cc69b
Mohamed Ashraf <mohammed.ashraaf.15@gmail.com>
2018-06-16 14:13:36 +0200

    Create Json format

---
+++ Formats/JsonFormat.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class JsonFormat implements FormatDataInterface
+{
+
+    /**
+     * Formats the data for different usages
+     * @param $data string as json
+     * @return array
+     */
+    public function format($data)
+    {
+        // convert the xml to an array with key, value pairs
+        return json_decode($data);
+    }
+}
